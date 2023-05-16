37989e92d21d68a3a82c7add1f2897d638dc3650
marco caneva <marco.caneva.dev@outlook.com>
2023-05-16 16:06:00 +0200

    fixed es6
    needs __str__ and self test

--- es6.py
+++ es6.py
@@ -1,4 +1,5 @@
 class Numbers:
+    MULTIPLIER = 0.8
     def __init__(self, x, y):
         if not isinstance(x, (int,float)):
             raise ValueError
@@ -6,3 +7,24 @@ class Numbers:
             raise ValueError
         self._x = x
         self._y = y
+
+    def add(self):
+        return self._x +self._y
+
+    def getValues(self):
+        return (self._x,self._y)
+
+    @classmethod
+    def multiply(cls,a):
+        if not isinstance(a, (int,float)):
+            raise ValueError
+        return a * cls.MULTIPLIER
+
+    @staticmethod
+    def subtract(a,b):
+        if not isinstance(a, (int,float)):
+            raise ValueError
+        if not isinstance(b, (int,float)):
+            raise ValueError
+        return b - c
+        
