8916888d680ed67d1302bceb5d99a8a6cc6e79bd
marco caneva <marco.caneva.dev@outlook.com>
2023-05-17 12:55:45 +0200

    fix es 7 not all

--- es7.py
+++ es7.py
@@ -1,2 +1,26 @@
 class Pizza:
-    def __init__
+
+    pizzarecipes = {'margherita':['pomodoro','mozzarella','basilico'],'diavola':['pomodoro','mozzarella','salamepicc']}
+    def __init__(self, ingredients):
+        self._ingredients = ingredients
+        
+    @classmethod
+    def getIngredients(cls, recipe):
+        try :
+            return cls.pizzarecipes[recipe]
+        except Exception:
+            raise ValueError
+    @classmethod
+    def fromRecipe(cls, recipe):
+        try:
+            return Pizza(cls.pizzarecipes[recipe])
+        except:
+            raise ValueError
+
+    def __str__(self):
+        return f'Delicious Bessa {self._ingredients!s}'
+    
+    
+
+if __name__ == '__main__':
+    negro = Pizza(['pomodoro', 'basilico', 'mozzarella'])
